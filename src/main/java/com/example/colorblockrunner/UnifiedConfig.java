97e5fdda4df72ef3f1c03f4a857cbba3b523d3d8
kmgacdby <kmgacdbyj@gmail.com>
2026-09-24 14:16:07 +0800

    Add configurable unified menu key

--- src/main/java/com/example/colorblockrunner/UnifiedConfig.java
+++ src/main/java/com/example/colorblockrunner/UnifiedConfig.java
@@ -9,7 +9,7 @@ public final class UnifiedConfig {
  private static final Gson GSON=new GsonBuilder().setPrettyPrinting().create();
  private static final Path FILE=FabricLoader.getInstance().getConfigDir().resolve("utility-suite.json");
  public boolean stewEnabled=false,runnerEnabled=false,dodgeEnabled=false;
- public int stewDelay=3,runnerRange=32,dodgeRange=24;
+ public int menuKey=79,stewDelay=3,runnerRange=32,dodgeRange=24;
  public boolean runnerJump=true,runnerSprint=true,dodgeJump=true,dodgeSneak=false,ignoreOwnArrows=true,showLanding=true;
  public int stewKey=72,runnerKey=74,dodgeKey=75;
  private static UnifiedConfig instance;
